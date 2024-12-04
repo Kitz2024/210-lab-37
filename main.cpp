887077df4a3d107c19aecc391d1e44ad490d7a20
Kitz2024 <kit.pollinger@gmail.com>
2024-12-03 18:19:23 -0800

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+// Kit Pollinger
+//  210 Lab 37 | Hash Table
+#include <iostream>
+#include <string>  // For string
+using namespace std;
+
+int main()
+{
+    cout << "Demo Code:\n";
+    char a = 'A';
+    cout << a << endl;
+    cout << (int)a << endl;
+    int b = 66;
+    cout << b << endl;
+    cout << (char)b << endl;
+    
+    return 0;
+}
+/*
+These targets are present in the dataset and can be used for testing:
+536B9DFC93AF
+1DA9D64D02A0
+666D109AA22E
+E1D2665B21EA
+*/
