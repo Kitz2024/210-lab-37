2c93505d6259d1d0b621e86f6c669a43fc2bdcb1
Kitz2024 <kit.pollinger@gmail.com>
2024-12-05 17:26:19 -0800

    Part 3 Start

--- main.cpp
+++ main.cpp
@@ -3,10 +3,12 @@
 #include <iostream>
 #include <string>  // For string
 #include <fstream> // For Part 2 and 3
+#include <map> // For part 3
+#include <list> // For Part 3
 using namespace std;
 
 // Function to calculate the sum of ASCII values in a string
-int sum_ascii(const string &str)
+int gen_hash_index(const string &str)
 {
     int sum = 0;
     for (char ch : str)
@@ -18,25 +20,8 @@ int sum_ascii(const string &str)
 
 int main()
 {
-    cout << "Demo Code:\n";
-    char a = 'A';
-    cout << a << endl;
-    cout << (int)a << endl;
-    int b = 66;
-    cout << b << endl;
-    cout << (char)b << endl;
-
-    // Testing the sum_ascii function
-    string AsciiTest;
-    cout << "Part 1: \n";
-    cout << "Enter a test string: ";
-    cin >> AsciiTest;
-
-    // Output the ASCII sum
-    cout << "String: " << AsciiTest << endl;
-    cout << "ASCII Sum: " << sum_ascii(AsciiTest) << endl;
-
-    cout << "Part 2: \n";
+   //Removed Demo Code and Part 1 - 2
+   cout << "Part 3: \n";
 
     const string filename = "lab-37-data.txt"; // Name of the dataset file
     ifstream infile(filename);                 // Open the file for reading
@@ -46,20 +31,6 @@ int main()
         cout << "Error: Could not open file!" << endl;
         return 1;
     }
-
-    string codeLine;
-    long long lineTotal = 0; // Use long long for large numbers
-
-    // Read each line from the file, calculate ASCII sum, and add to the grand total
-    while (infile >> codeLine)
-    {
-        lineTotal += sum_ascii(codeLine);
-    }
-
-    infile.close(); // Close the file
-
-    // Output the  total ASCII Sum from File
-    cout << " Total ASCII Sum: " << lineTotal << endl;
     return 0;
 }
 /*
